a54b40234f4c0634202ee67565aee8d08ba0d901
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-08-31 22:22:34 +0300

    v2

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request) :
-    return HttpResponse("Hello Karim")
+    return HttpResponse("Hello Django")
 # Create your views here.
