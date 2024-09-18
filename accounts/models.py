ffc4c713d9387dc5cb0e71368a4d554c7b0c2e5f
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-09-18 19:09:55 +0300

    v13.1.19

--- accounts/models.py
+++ accounts/models.py
@@ -8,6 +8,10 @@ class Profile(models.Model) :
     squad=models.JSONField(default={"squad":[]})
     squad_name=models.CharField(null=True,blank=True)
     current_balance=models.IntegerField(default=12)
+    wildcard=models.BooleanField(default=True)
+    freehit=models.BooleanField(default=True)
+    tripplecaptin=models.BooleanField(default=True)
+    availiable_tarnsefere=models.IntegerField(default=35)
     def __str__(self) -> str:
         return self.user.username
 # Create your models here.
