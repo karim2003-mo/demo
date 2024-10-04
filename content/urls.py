ebf89de974b062e6b3e54ad0e4eb4e0afe8aeca0
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-10-04 07:52:52 +0300

    v13.1.25

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from . import views2
 urlpatterns = [
     path('teams/',views.all_teams,name="all_teams"),
     path("<team>/players/",views.view_player,name="palyers"),
@@ -12,5 +11,4 @@ urlpatterns = [
     path("grep_events/",views.made_json_teams,name="grep_events"),
     path("get_public_info/",views.get_public_info,name="get_public_info"),
     path("player_points/<str:team>/<str:type>/",views.player_points,name="player_points"),
-    path("post_players_point/",views2.post_players_point,name="post_players_point"),
 ]
