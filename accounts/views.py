7677fdf7f2b9fc00f9cfc336e8ed85dbaf864d70
Karim Gamal Elsayed <kareemabolisa@gmail.com>
2024-10-11 14:25:27 +0300

    v13.1.26

--- accounts/views.py
+++ accounts/views.py
@@ -66,7 +66,7 @@ def signup(request) :
             profile=Profile.objects.create(user=user)
             user.save()
             profile.save()
-            return redirect(verify_account,email_address=email)
+            return redirect('verify_account',email_address=email)
         except:
             return JsonResponse({"status":"error"})
     else :
